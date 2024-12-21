269f8385e823305163411553fb566c3fac8021c8
ja.zoe <j.vilfort01@gmail.com>
2024-12-21 18:02:50 -0500

    Initial commit

---
+++ src/app/get-started/page.tsx
@@ -0,0 +1,8 @@
+const GetStarted = () => {
+  return (
+    <div className="h-screen flex flex-col justify-center items-center">
+        <h1>Justin Fermin a bitch 😝</h1>
+    </div>
+  )
+}
+export default GetStarted
